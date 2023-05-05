21e279c0a13e16f6ecac1bbdf82df160819cd856
juanma <juanaco1.p@gmail.com>
2023-05-05 18:47:22 +0200

    layout styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return (<div>hola</div>)
+  return (
+    <main className="bg-red-500">
+        <div className="bg-green-500 container">
+            <h1 className="text-4xl text-center">Hello World</h1>
+        </div>
+    </main>
+  )
 }
