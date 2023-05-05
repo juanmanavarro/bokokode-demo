d6153a89c159abf45f8a13c212e68c168c1e7d7f
juanma <juanaco1.p@gmail.com>
2023-05-05 19:38:33 +0200

    main product header styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ export default function Home() {
   return (
     <main className="container">
         <header className="header">
-            <h1 className="text-xl font-bold">BEJAMAS_</h1>
+            <h1 className="h1">BEJAMAS_</h1>
             <a href="">c</a>
         </header>
+        <section className="main-product mt-5">
+            <div className="main-product-header">
+                <h2 className="h2">Samurai King Resting</h2>
+                <button className="button-black">Add to cart</button>
+            </div>
+        </section>
     </main>
   )
 }
