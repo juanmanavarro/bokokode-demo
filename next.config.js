4183356f101f8eb154b708de3531e0f172498de1
juanma <juanaco1.p@gmail.com>
2023-05-05 18:33:40 +0200

    sass working

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const path = require('path');
+
+console.log(path.join(__dirname, 'styles'));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 
 module.exports = nextConfig
