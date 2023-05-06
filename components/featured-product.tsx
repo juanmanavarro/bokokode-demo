a200764628f84bca2076080f9195310d2bae9af4
juanma <juanaco1.p@gmail.com>
2023-05-06 11:40:06 +0200

    description

--- components/featured-product.tsx
+++ components/featured-product.tsx
@@ -2,7 +2,7 @@ export default function FeaturedProduct({ product }: { product: any }) {
     return (
         <section className="fp mt-5 md:mt-10">
             <div className="flex justify-between items-center py-2">
-                <h2 className="font-bold text-2xl max-w-md md:max-w-full">{product?.name}</h2>
+                <h2 className="font-bold text-2xl md:text-3xl max-w-md md:max-w-full">{product?.name}</h2>
                 <button className="button-black hidden md:block">Add to cart</button>
             </div>
             <div className="fp-image">
@@ -10,10 +10,10 @@ export default function FeaturedProduct({ product }: { product: any }) {
                 <div className="caption">Photo of the day</div>
             </div>
             <button className="button-black w-full md:hidden">Add to cart</button>
-            <div className="fp-footer">
-                <div className="fp-description">
-                    <h4>About the {product?.name}</h4>
-                    <p>{product?.category}</p>
+            <div className="pt-3">
+                <div className="">
+                    <h4 className="font-bold mb-2 md:text-2xl">About the {product?.name}</h4>
+                    <p className="capitalize hidden md:block">{product?.category}</p>
                     <p>{product?.description}</p>
                 </div>
                 <div className="fp-related">
