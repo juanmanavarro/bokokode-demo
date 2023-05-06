05d01607a1af5e33f661301ec6c2760bc9d59c3a
juanma <juanaco1.p@gmail.com>
2023-05-06 11:34:44 +0200

    fp header

--- components/featured-product.tsx
+++ components/featured-product.tsx
@@ -1,14 +1,15 @@
 export default function FeaturedProduct({ product }: { product: any }) {
     return (
-        <section className="fp mt-10">
-            <div className="fp-header">
-                <h2 className="h2">{product?.name}</h2>
-                <button className="button-black">Add to cart</button>
+        <section className="fp mt-5 md:mt-10">
+            <div className="flex justify-between items-center py-2">
+                <h2 className="font-bold text-2xl max-w-md md:max-w-full">{product?.name}</h2>
+                <button className="button-black hidden md:block">Add to cart</button>
             </div>
             <div className="fp-image">
                 <img src={product?.image.src} alt={product?.image.alt} />
                 <div className="caption">Photo of the day</div>
             </div>
+            <button className="button-black w-full md:hidden">Add to cart</button>
             <div className="fp-footer">
                 <div className="fp-description">
                     <h4>About the {product?.name}</h4>
