29b48db12dcf968e3f1ebe31c63e00a24ac42553
juanma <juanaco1.p@gmail.com>
2023-05-08 17:38:27 +0200

    filter working

--- app/hooks/useApi.ts
+++ app/hooks/useApi.ts
@@ -3,8 +3,10 @@ import axios from 'axios'
 const baseUrl = 'https://technical-frontend-api.bokokode.com/api'
 
 export const useFetchProducts = async ({ page = 1, categories = [] }: { page?: number, categories?: string[] }) => {
+  const body = categories.length > 0 ? { categories } : {};
+
   try {
-    const { data } = await axios.post(`${baseUrl}/products?page=${page}`)
+    const { data } = await axios.post(`${baseUrl}/products?page=${page}`, body)
     return data;
   } catch (error) {
     console.error(error);
--- components/products.tsx
+++ components/products.tsx
@@ -1,14 +1,33 @@
 import FilterItems from './filter/items';
 import Product from './product';
 import Filter from '@/components/filter/index';
-import { useGetProducts } from './useRequest';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useFetchProducts } from '@/app/hooks/useApi';
 
 export default function Products() {
+    const [loading, setLoading] = useState<boolean>(true)
     const [pageIndex, setPageIndex] = useState(0)
-    const { data, isLoading } = useGetProducts(pageIndex)
-    const products = data?.data.data
-    const paginationLinks = data?.data.links
+    const [categories, setCategories] = useState<string[]>([])
+    const [products, setProducts] = useState<any[]>([])
+    const [paginationLinks, setPaginationLinks] = useState<string[]>([])
+
+    useEffect(() => {
+        useFetchProducts({ categories, page: pageIndex })
+            .then(res => {
+                setProducts(res?.data.data)
+                setPaginationLinks(res?.data.links)
+                setLoading(false)
+            })
+    }, [categories, pageIndex])
+
+    const setFilter = async(category: string) => {
+        if ( categories.includes(category) ) {
+            setCategories(categories.filter(cat => cat !== category))
+        } else {
+            setCategories([...categories, category])
+        }
+        setPageIndex(1)
+    }
 
     return (
         <section className="relative">
@@ -26,39 +45,41 @@ export default function Products() {
                 <aside className="p-2 hidden md:block">
                     <h5 className="title-3">Category</h5>
                     <ul className="mt-3">
-                        {/* <FilterItems /> */}
+                        <FilterItems onFilter={setFilter} />
                     </ul>
                 </aside>
                 <div className="col-span-2 md:p-2">
-                    <div className="grid grid-cols-1 gap-14 md:grid-cols-3 md:gap-4">
-                        {products?.map((product: any) => (
-                            <Product product={product} key={product._id} />
-                        ))}
-                    </div>
-                    <div className="pagination flex justify-center align-center mt-10">
-                        <a
-                            onClick={() => setPageIndex(Number(pageIndex <= 1 ? pageIndex : pageIndex - 1))}
-                            className="mx-1 cursor-pointer font-bold"
-                        >
-                            {'<'}
-                        </a>
-                        {paginationLinks?.slice(1, -1).map((link: any) => (
+                        {loading ? <h1>loading</h1> : (
+                            <div className="grid grid-cols-1 gap-14 md:grid-cols-3 md:gap-4">
+                                {products?.map((product: any) => (
+                                    <Product product={product} key={product._id} />
+                                ))}
+                            </div>
+                        )}
+                        <div className="pagination flex justify-center align-center mt-10">
                             <a
-                                onClick={() => setPageIndex(Number(link.label))}
-                                className={`mx-1 cursor-pointer ${link.active ? 'font-bold' : 'text-gray-500'}`}
-                                key={link.label}
+                                onClick={() => setPageIndex(Number(pageIndex <= 1 ? pageIndex : pageIndex - 1))}
+                                className="mx-1 cursor-pointer font-bold"
                             >
-                                {link.label}
+                                {'<'}
                             </a>
-                        ))}
-                        <a
-                            onClick={() => setPageIndex(Number(pageIndex >= 9 ? pageIndex : pageIndex + 1))}
-                            className="mx-1 cursor-pointer font-bold"
-                        >
-                            {'>'}
-                        </a>
+                            {paginationLinks?.slice(1, -1).map((link: any) => (
+                                <a
+                                    onClick={() => setPageIndex(Number(link.label))}
+                                    className={`mx-1 cursor-pointer ${link.active ? 'font-bold' : 'text-gray-500'}`}
+                                    key={link.label}
+                                >
+                                    {link.label}
+                                </a>
+                            ))}
+                            <a
+                                onClick={() => setPageIndex(Number(pageIndex >= 9 ? pageIndex : pageIndex + 1))}
+                                className="mx-1 cursor-pointer font-bold"
+                            >
+                                {'>'}
+                            </a>
+                        </div>
                     </div>
-                </div>
             </div>
         </section>
     )
