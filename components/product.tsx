4bc46b4d6535730d62ee1601773ba99394921f6b
juanma <juanaco1.p@gmail.com>
2023-05-08 11:36:55 +0200

    show button on hover

--- components/product.tsx
+++ components/product.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 export default function Product({ product, height }: { product: any, height?: string }) {
+    const [showAddToCard, setShowAddToCart] = useState(false);
+
     return (
         <div className="max-w-full" key={product._id}>
             <div
-                className="w-full h-full max-w-full"
+                className="w-full h-full max-w-full relative"
+                onMouseOver={(e) => setShowAddToCart(true)}
+                onMouseLeave={(e) => setShowAddToCart(false)}
                 style={{
                     backgroundImage: `url(${product.image.src})`,
                     height: height || '400px',
@@ -12,6 +18,9 @@ export default function Product({ product, height }: { product: any, height?: st
                     backgroundRepeat: 'no-repeat'
                 }}
             >
+                <button className={`button-black truncate w-full absolute inset-x-0 bottom-0 ${showAddToCard ? 'block' : 'hidden'}`}>
+                    Add to cart
+                </button>
             </div>
             <div className="py-2">
                 <div className="capitalize text-sm text-gray-500">{product.category}</div>
