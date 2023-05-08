dc5222ae61c9ad6e65d6fdb6d29c50674bf4f125
juanma <juanaco1.p@gmail.com>
2023-05-08 11:55:37 +0200

    spacing

--- components/featured-product.tsx
+++ components/featured-product.tsx
@@ -3,23 +3,25 @@ import Product from "./product";
 export default function FeaturedProduct({ product }: { product: any }) {
     return (
         <section className="fp mt-5 md:mt-10 border-b-4 pb-10 mb-10">
-            <div className="flex justify-between items-center py-2">
-                <h2 className="font-bold text-2xl md:text-3xl max-w-md md:max-w-full">{product?.name}</h2>
+            <div className="flex justify-between items-center py-4">
+                <h2 className="font-bold text-2xl md:text-3xl max-w-md md:max-w-full">
+                    {product?.name}
+                </h2>
                 <button className="button-black hidden md:block">Add to cart</button>
             </div>
-            <div className="fp-image relative">
+            <div className="fp-image relative mb-5">
                 <img src={product?.image.src} alt={product?.image.alt} />
                 <div className="caption bottom-0">Photo of the day</div>
             </div>
-            <button className="button-black w-full md:hidden">Add to cart</button>
+            <button className="button-black w-full md:hidden mb-5">Add to cart</button>
             <div className="pt-3 md:flex">
-                <div className="">
-                    <h4 className="font-bold mb-2 md:text-2xl">About the {product?.name}</h4>
+                <div>
+                    <h4 className="font-bold mb-2 md:text-2xl mb-5">About the {product?.name}</h4>
                     <p className="capitalize hidden md:block">{product?.category}</p>
                     <p>{product?.description}</p>
                 </div>
                 <div className="md:w-2/5 md:ms-3">
-                    <h4 className="font-bold text-xl mb-3 mt-5 md:mt-0">People also buy</h4>
+                    <h4 className="font-bold text-xl mb-3 mt-5 md:mt-0 mb-5">People also buy</h4>
                     <div className="grid grid-cols-3 gap-2">
                         {product?.people_also_buy.map((related: any) => (
                             <div className="related-product" key={related._id}>
